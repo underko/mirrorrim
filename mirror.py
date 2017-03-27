975017c1ac44c9544e0162bd7cc6b4156c5d6e35
Martin Janocko <martin.janocko.gih@gmail.com>
2017-03-27 23:04:49 +0200

    Added mirror module

--- mirror.py
+++ mirror.py
@@ -2,14 +2,34 @@
 
 import subprocess
 import time
+from gpiozero import MotionSensor
+
+WORDS = ["TRAM", "BUS", "PUBLIC", "TRANSPORT", "TIMETABLE", "SCHEDULE"]
+PRIORITY = 10
 
 time_night_start = 22
 time_night_end = 8
 
+pir = MotionSensor(4)
+
+print("Debug: Starting")
+
 while True:
     current_time = time.localtime
+    print("Debug: current_time: {0}".format(current_time))
     if current_time.tm_hour >= time_night_start || current_time.tm_hour <= time_night_end:
-        # todo night mode
+        print("Debug: Night mode.")
+        if (pir.motion_detected):
+            print("Debug: Night mode. Motion detected.")
+            set_night_mode()
+        else:
+            print("Debug: Night mode. Screen off.")
+            set_screen_off()
+    else:
+        print("Debug: Default screen.")
+        set_default_mode()
+
+    time.sleep(1)
 
 def set_default_mode():
     focus_window("Mirrorrim - Default Screen")
@@ -21,10 +41,18 @@ def set_night_mode():
     focus_window("Mirrorrim - Night screen")
 
 def set_screen_off():
-    run_command
+    print("Debug: Screen turned off TMP function.")
 
 def focus_window(title):
     run_command("wmctrl - \"{0}\"".format(title))
 
 def run_command(command):
     subprocess.Popen(command)
+
+def isValid(text):
+    return any(word.lower() in text.lower() for word in WORDS)
+
+def handle(text, mic, profile):
+    print("Debug: Displaying IMHD table.")
+    set_imhd_timetable_mode()
+    time.sleep(5)
